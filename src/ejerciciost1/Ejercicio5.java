2a49b66107418a0a19609a870b10cf87615a6a13
Elena <Elena@106-01.info2>
2023-09-26 13:56:38 +0200

    Ejercicio 5 corregido

--- src/ejerciciost1/Ejercicio5.java
+++ src/ejerciciost1/Ejercicio5.java
@@ -1,10 +1,39 @@
 package ejerciciost1;
 
+import java.util.Scanner;
+
 public class Ejercicio5 {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Variable donde guardar el radio del círculo
+		float radio;
+		
+		// Variable donde guardar la longitud del círculo
+		double longitud;
+		
+		// Variable donde guardar el área del círculo
+		double area;
+		
+		// Scanner para leer del teclado
+		Scanner sc = new Scanner(System.in);
+		
+		// Le pido al usuario que me introduzca un radio
+		System.out.println("Introduzca el radio del círculo");
+		
+		// Leemos el radio de teclado
+		radio = sc.nextFloat();
+		
+		// Calculamos la longitud
+		longitud = 2 * Math.PI * radio;
+		
+		// Calculamos el área
+		area = Math.PI * Math.pow(radio, 2);
+		
+		// Mostramos la longitud y el área
+		System.out.println("La longitud es " + longitud + " y el área es " + area);
+		
+		// Cerramos el Scanner
+		sc.close();
 	}
 
 }
